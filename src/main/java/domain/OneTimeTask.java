86cc6384b47e0b98ba39b67f9b75c8b0b2aea580
kuan1701 <77536869+kuan1701@users.noreply.github.com>
2021-01-30 23:15:51 +0300

    Add files via upload

---
+++ src/main/java/domain/OneTimeTask.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class OneTimeTask extends Task{
+	
+	//Constructors
+	public OneTimeTask(String name, int date, String type, String category, String priority, String description, boolean state, int deadline) {
+		super(name, date, type, category, priority, description, state, deadline);
+	}
+	
+	
+}
